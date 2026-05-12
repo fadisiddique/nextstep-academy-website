a979002ba44a0b23c159fe1e37b43c251881e0d9
Fadi Siddique <fadisiddique@gmail.com>
2026-05-12 18:27:54 +0530

    Fix production CORS origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,10 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
     "http://localhost:3000",
     "https://nextstepacademyonline.in",
     "https://www.nextstepacademyonline.in",
+    "https://nextstep-academy-website.vercel.app"
   ],
   credentials: true,
 }));
